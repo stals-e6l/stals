087e9d501e7780d89e2bc2f476d0d569876e9e39
Benedict Sy <bsy@up.edu.ph>
2023-03-19 14:47:41 +0800

    feat: added provisional report schema

---
+++ database/nosql/report.js
@@ -0,0 +1,29 @@
+/* Subject to review */
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema({
+    timeStamp: { 
+        type: Date, required: true 
+    },
+    userReference: { // Sort of like a foreign key, user._id
+        type: String, required: true 
+    },
+    userName: {
+        type: String, required: true 
+    },
+    filters: { // Accommodation type, location, price range, etc.
+        tags: [String], location: String , priceRange : String
+    },
+    reportContent: { // Accommodation references/accommodation._id that fit the filter (not certain about this)
+        type: Array, default : [] 
+    }, 
+});
+
+module.exports = mongoose.model("Report", reportSchema);
+
+/* Notes 
+
+- User model should probably have a list/array of reports they generated (report._id)
+- Very much open to suggestions on what to add/remove/update
+
+*/
