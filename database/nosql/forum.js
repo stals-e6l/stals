3e7c6158507fe4e6b05413f64a7dbfc284df0596
jtdolendo <jtdolendo@up.edu.ph>
2023-03-21 20:49:54 +0800

    feat: added forum schema

---
+++ database/nosql/forum.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const forumSchema = new mongoose.Schema({
+    title: {
+        type: String, required: true
+    },
+    content: {
+        type: Array, default: [], required: true
+    },
+    status: {
+        type: String, enum: ['active', 'archived', 'deleted'], required: true
+    },
+    userReference: {
+        type: Number, required: true
+    },
+    accomReference: {
+        type: Number, required: true
+    }
+});
+
+module.exports = mongoose.model("Forum", forumSchema);
