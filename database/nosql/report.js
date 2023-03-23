014725653e1160e705fecced3b59b279a8bf35cf
Benedict Sy <bsy@up.edu.ph>
2023-03-23 22:14:48 +0800

    feat: updated report schema

--- database/nosql/report.js
+++ database/nosql/report.js
@@ -1,29 +1,17 @@
-/* Subject to review */
+/* Updated schema */
 const mongoose = require("mongoose");
 
-const reportSchema = new mongoose.Schema({
-    timeStamp: { 
-        type: Date, required: true 
+const reportSchema = new mongoose.Schema({ 
+    timestamps: true, 
+    user_id: { // Sort of like a foreign key, user._id
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true 
     },
-    userReference: { // Sort of like a foreign key, user._id
-        type: String, required: true 
-    },
-    userName: {
-        type: String, required: true 
-    },
-    filters: { // Accommodation type, location, price range, etc.
-        tags: [String], location: String , priceRange : String
-    },
-    reportContent: { // Accommodation references/accommodation._id that fit the filter (not certain about this)
-        type: Array, default : [] 
+    pdf_url: { // Used to generate a pdf for the report
+        type: String, 
+        required: true
     }, 
 });
 
 module.exports = mongoose.model("Report", reportSchema);
-
-/* Notes 
-
-- User model should probably have a list/array of reports they generated (report._id)
-- Very much open to suggestions on what to add/remove/update
-
-*/
