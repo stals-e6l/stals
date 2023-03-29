a4e6b16ec296ed3059b13d4a4c5fd8542f0869cc
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-03-29 18:38:22 +0800

    feat: accommodation types

---
+++ frontend/src/types/accommodation.d.ts
@@ -0,0 +1,40 @@
+type TAccommodationType =
+  | 'hotel'
+  | 'apartment'
+  | 'bedspace'
+  | 'dormitory'
+  | 'transient'
+type TAccommodationFurnishing =
+  | 'unfurnished'
+  | 'semifurnished'
+  | 'fully_furnished'
+
+interface IAccommodation {
+  // nullable
+  _id?: string // why? this is only available when created
+  owner_id?: string // why? same with _id
+  description?: string // an accommodation can have no description
+  // required
+  name: string
+  address: string
+  type: TAccommodationType
+  price: number
+  size_sqm: number
+  meters_from_uplb: number
+  landmarks: string[]
+  min_pax: number
+  max_pax: number
+  num_rooms: number
+  num_beds: string // e.g. 2-3 beds
+  num_views: number
+  furnishing: TAccommodationFurnishing
+  cooking_rules: string[]
+  pet_rules: string[]
+  other_rules: string[]
+  safety_and_security: string[]
+  appliances: string[]
+  amenities: string[]
+  created_at: string
+  updated_at: string
+  is_soft_deleted: boolean
+}
