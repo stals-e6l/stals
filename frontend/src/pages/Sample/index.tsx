7a8c892af3162550c1a79bfe54f0576f45f75bcf
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-06 01:39:22 +0800

    feat: add sample apiGet

--- frontend/src/pages/Sample/index.tsx
+++ frontend/src/pages/Sample/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSample } from '../../store/sample'
+import { apiGet } from '../../api'
 
 interface IProps {
   children?: React.ReactNode
@@ -8,9 +9,22 @@ interface IProps {
 const SamplePage: React.FC<IProps> = () => {
   const { data, dispatch: sampleDispatch } = useSample()
 
+  const [ping, setPing] = React.useState<IResponse<string> | null>(null)
+
+  const doPing = async () => {
+    const res = await apiGet<string>('http://localhost:5000/api/ping')
+    return res
+  }
+
+  React.useEffect(() => {
+    doPing().then(data => {
+      setPing(data)
+    })
+  }, [])
+
   return (
     <div data-testid="SamplePage">
-      {JSON.stringify(data)}
+      {JSON.stringify(ping && ping.data)}
       <button
         onClick={() =>
           sampleDispatch({
