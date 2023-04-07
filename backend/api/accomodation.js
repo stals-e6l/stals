2c8c038e964792408bcd9a4450a6da900bf2ff12
nesorinio <nesorinio@up.edu.ph>
2023-04-07 21:07:39 +0800

    refactor: return newly edited accommodation instead of old one

--- backend/api/accomodation.js
+++ backend/api/accomodation.js
@@ -171,7 +171,8 @@ accom.put('/:id', async function(req, res){
     try{
         const editedAccom = await Accomodation.findOneAndUpdate(
             {_id: req.params.id},
-            {name: req.body.name});
+            {name: req.body.name},
+            {new: true});
         res.status(200).json({ success: true, data: editedAccom })
     } catch(err){
         switch(err){
