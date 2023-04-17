7cd0a0da7982eff47152084425e77a426833d5c2
eunametal <eunametal@pop-os.localdomain>
2023-04-17 20:20:22 +0800

    commit

---
+++ frontend/src/pages/Accommodation/delete.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { deleteAccommodation, retrieveAccommodationById } from '../../store/accommodation/actions'
+import { Box, Button, TextField, Dialog } from '@mui/material'
+
+interface AccomDetails extends IAcommodation {
+    open: boolean;
+    handleClose: () => void;
+}
+
+const DeleteAccomodation = (props: AccomDetails) => {
+    return <>
+        <Button onClick={handleClickOpen}>Delete</Button>
+        <Dialog 
+        open={props.open}
+        handleClose={props.handleClose}
+        >
+        </Dialog>
+    </>
+}
+
+export default DeleteAccomodation 
--- frontend/src/pages/Accommodation/detail.tsx
+++ frontend/src/pages/Accommodation/detail.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import {
-  deleteAccommodation,
-  retrieveAccommodationById,
-  updateAccommodation,
-} from '../../store/accommodation/actions'
+import { useParams } from 'react-router-dom'
+import { retrieveAccommodationById } from '../../store/accommodation/actions'
 
 interface IProps {
   children?: React.ReactNode
@@ -12,43 +8,9 @@ interface IProps {
 
 const AccommodationDetailPage: React.FC<IProps> = () => {
   const params = useParams()
-  const navigate = useNavigate()
   const accommodation = retrieveAccommodationById(params.id as string)
-  const updateAccommodationHandler = updateAccommodation()
-  const deleteAccommodationHandler = deleteAccommodation()
 
-  if (!accommodation) {
-    return <div>no accommodation found!</div>
-  }
-
-  return (
-    <div>
-      {JSON.stringify(accommodation)}
-      <button
-        onClick={() => {
-          if (accommodation) {
-            updateAccommodationHandler({ ...accommodation, name: 'STUFF' })
-          }
-        }}
-      >
-        click to update its name to STUFF
-      </button>
-      <button
-        onClick={async () => {
-          try {
-            if (accommodation._id) {
-              await deleteAccommodationHandler(accommodation._id)
-              navigate('/accommodations')
-            }
-          } catch (err) {
-            alert('error deleting accommodation!')
-          }
-        }}
-      >
-        click to delete accommodation with id {accommodation?._id}
-      </button>
-    </div>
-  )
+  return <div>{JSON.stringify(accommodation)}</div>
 }
 
 export default AccommodationDetailPage
