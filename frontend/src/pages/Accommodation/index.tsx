4139c1408b4b9a8b43a2075117e4d265184f7087
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-18 16:08:51 +0800

    feat: add QuickCreateAccommodation

--- frontend/src/pages/Accommodation/index.tsx
+++ frontend/src/pages/Accommodation/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {
-  createAccommodation,
-  retrieveAccommodations,
-} from '../../store/accommodation/actions'
-import { Button } from '@mui/material'
-import UpdateAccomodation from './update/UpdateAccomodation'
+import { retrieveAccommodations } from '../../store/accommodation/actions'
+import QuickCreateAccommodation from './dev/QuickCreateAccommodation'
 
 interface IProps {
   children?: React.ReactNode
@@ -14,41 +10,10 @@ interface IProps {
 const AccommodationPage: React.FC<IProps> = () => {
   const accommodations = retrieveAccommodations()
   const navigate = useNavigate()
-  const createAccommodationHandler = createAccommodation()
 
   return (
     <div>
-      <button
-        onClick={() => {
-          createAccommodationHandler({
-            name: 'test name',
-            description: 'listing description',
-            address: 'listing address',
-            type: 'apartment',
-            price: 0,
-            size_sqm: 0,
-            meters_from_uplb: 0,
-            landmarks: [],
-            min_pax: 0,
-            max_pax: 0,
-            num_rooms: 0,
-            num_beds: 0,
-            num_views: 0,
-            furnishing: 'semifurnished',
-            cooking_rules: [],
-            pet_rules: [],
-            other_rules: [],
-            safety_and_security: [],
-            appliances: [],
-            amenities: [],
-            created_at: 'date',
-            updated_at: 'date',
-            is_soft_deleted: false,
-          })
-        }}
-      >
-        create
-      </button>
+      <QuickCreateAccommodation />
       <ul>
         {accommodations.map((accommodation, key: number) => (
           <li
