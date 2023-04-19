b9abfcd51354d0dc7da28c62ccceb96f76ad0b36
jabarro <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>
2023-04-20 02:48:48 +0800

    feat: created delete accommodation modal

--- frontend/src/pages/Accommodation/delete.tsx
+++ frontend/src/pages/Accommodation/delete.tsx
@@ -1,22 +1,57 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { deleteAccommodation, retrieveAccommodationById } from '../../store/accommodation/actions'
-import { Box, Button, TextField, Dialog } from '@mui/material'
+import {
+  deleteAccommodation,
+  retrieveAccommodationById,
+} from '../../store/accommodation/actions'
+import {
+  Box,
+  Button,
+  TextField,
+  Dialog,
+  Typography,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from '@mui/material'
 
-interface AccomDetails extends IAcommodation {
-    open: boolean;
-    handleClose: () => void;
-}
+const DeleteAccom = () => {
+  const [open, setState] = React.useState(false)
 
-const DeleteAccomodation = (props: AccomDetails) => {
-    return <>
-        <Button onClick={handleClickOpen}>Delete</Button>
-        <Dialog 
-        open={props.open}
-        handleClose={props.handleClose}
-        >
-        </Dialog>
+  const handleClose = () => {
+    setState(false)
+  }
+
+  const handleOpen = () => {
+    setState(false)
+  }
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setState(true)
+        }}
+      >
+        Open
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Delete Accomodation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete accomodation?
+          </DialogContentText>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: '#000',
+            }}
+          />
+        </DialogContent>
+      </Dialog>
     </>
+  )
 }
 
-export default DeleteAccomodation 
+export default DeleteAccom
