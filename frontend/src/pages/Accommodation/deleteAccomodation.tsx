acf873c8f5311c7301aecda4fab8130a44c5c313
jabarro <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>
2023-04-20 09:42:38 +0800

    refactor: added components for deleteAccommodation

--- frontend/src/pages/Accommodation/deleteAccomodation.tsx
+++ frontend/src/pages/Accommodation/deleteAccomodation.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { deleteAccommodation, retrieveAccommodationById } from '../../store/accommodation/actions'
-import { Box, Button, TextField, Dialog, Typography, DialogTitle, DialogContent, DialogContentText } from '@mui/material'
+import { Box, Button, TextField, Dialog, Typography, DialogTitle, DialogContent, DialogContentText, Grid, DialogActions } from '@mui/material'
 
 const DeleteAccom = () => {
     const [open, setState] = React.useState(false)
@@ -18,15 +18,26 @@ const DeleteAccom = () => {
     <>
         <Button onClick={ () => { setState(true) } }>Open</Button>
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Delete Accomodation</DialogTitle>
-            <DialogContent>
-                <DialogContentText>Are you sure you want to delete accomodation?</DialogContentText>
-                <Box sx={{
-                    width: 300,
-                    height: 300,
-                    backgroundColor: '#000'
+            <Grid sx={{ flexGrow: 1 }} container spacing={0}>
+                <Grid item xs sx={{
+                    backgroundColor: '#696969'
+                }}>
+                    <Box sx={{
                     }} />
-            </DialogContent>
+                </Grid>
+                <Grid item xs sx={{
+                    padding: 5
+                }}>
+                
+                        <Typography variant="body1">Are you sure that you want to delete accommodation?</Typography>
+                        <TextField required placeholder="Confirm" ></TextField>
+                        <Typography variant="body1">Type Confirm to delete the listing.</Typography>
+
+                        <Button onClick={ () => { setState(false) } }>Cancel</Button>
+                        <Button onClick={ () => { setState(false) } }>Delete</Button>
+
+                </Grid>
+            </Grid>
         </Dialog>
     </>
            )
