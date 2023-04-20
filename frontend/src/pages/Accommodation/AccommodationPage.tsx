0f2269ceb3476715abe009cb8683c7fe82c83d89
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-20 13:38:10 +0800

    refactor: update Accommodations

--- frontend/src/pages/Accommodation/AccommodationPage.tsx
+++ frontend/src/pages/Accommodation/AccommodationPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
 import Banner from '../../components/bannerElement'
 import Header from '../../components/header'
-import { colors, fontFamily } from '../../theme'
 import Accommodations from './Accommodations'
 
 interface IProps {
@@ -14,35 +12,8 @@ const AccommodationPage: React.FC<IProps> = () => {
     <div id="retrieve-all">
       <Header />
       <Banner />
-      <Box id="Retrieve-All-BoxGrid">
-        <Typography
-          sx={{
-            color: 'black',
-            fontFamily: fontFamily,
-            fontWeight: 'bold',
-            fontSize: '25px',
-            display: 'flex',
-            marginTop: '2%',
-            marginLeft: '10%',
-          }}
-        >
-          {' '}
-          <Typography
-            sx={{
-              color: colors.green,
-              fontFamily: 'inherit',
-              fontWeight: 'inherit',
-              fontSize: 'inherit',
-              marginRight: '0.25%',
-            }}
-          >
-            |
-          </Typography>{' '}
-          Most Viewed
-        </Typography>
 
-        <Accommodations />
-      </Box>
+      <Accommodations />
     </div>
   )
 }
--- frontend/src/pages/Accommodation/Accommodations.tsx
+++ frontend/src/pages/Accommodation/Accommodations.tsx
@@ -1,7 +1,8 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, Typography, colors } from '@mui/material'
 import React from 'react'
 import AccommodationTile from '../../components/accommTile'
 import { retrieveAccommodations } from '../../store/accommodation/actions'
+import { fontFamily } from '../../theme'
 
 interface IProps {
   children?: React.ReactNode
@@ -11,31 +12,59 @@ const Accommodations: React.FC<IProps> = () => {
   const accommodations = retrieveAccommodations()
 
   return (
-    <Grid
-      container
-      id="Retrieve-All-Grid"
-      rowGap={2}
-      columnGap={2}
-      sx={{
-        alignContent: 'center',
-        justifyContent: 'center',
-        paddingTop: '20px',
-        transition: '0.3s all',
-      }}
-    >
-      {accommodations.map((accommodation, key: number) => (
-        <Grid item lg={3} sx={{}} key={key}>
-          <AccommodationTile
-            type={accommodation.type}
-            rating={4.5}
-            name={accommodation.name}
-            price={accommodation.price}
-            review_num={20}
-            link={`/accommodations/${accommodation._id}`}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Box id="Retrieve-All-BoxGrid">
+      <Typography
+        sx={{
+          color: 'black',
+          fontFamily: fontFamily,
+          fontWeight: 'bold',
+          fontSize: '25px',
+          display: 'flex',
+          marginTop: '2%',
+          marginLeft: '10%',
+        }}
+      >
+        {' '}
+        <Typography
+          sx={{
+            color: colors.green,
+            fontFamily: 'inherit',
+            fontWeight: 'inherit',
+            fontSize: 'inherit',
+            marginRight: '0.25%',
+          }}
+        >
+          |
+        </Typography>{' '}
+        Most Viewed
+      </Typography>
+
+      <Grid
+        container
+        id="Retrieve-All-Grid"
+        rowGap={2}
+        columnGap={2}
+        sx={{
+          alignContent: 'center',
+          justifyContent: 'center',
+          paddingTop: '20px',
+          transition: '0.3s all',
+        }}
+      >
+        {accommodations.map((accommodation, key: number) => (
+          <Grid item lg={3} sx={{}} key={key}>
+            <AccommodationTile
+              type={accommodation.type}
+              rating={4.5}
+              name={accommodation.name}
+              price={accommodation.price}
+              review_num={20}
+              link={`/accommodations/${accommodation._id}`}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
