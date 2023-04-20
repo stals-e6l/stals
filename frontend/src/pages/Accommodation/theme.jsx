efaaa7232ad66f45e4395c5988a2ccf87149beb9
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-20 13:41:09 +0800

    chore: prettier

--- frontend/src/pages/Accommodation/theme.jsx
+++ frontend/src/pages/Accommodation/theme.jsx
@@ -1,32 +1,31 @@
-import { createTheme } from "@mui/material"
+import { createTheme } from '@mui/material'
 
 export const theme = createTheme({
-    palette: {
-        textColor: {
-            main: "#1d1d15",         // black
-            subMain: "#154360",      // light gray
-            subHeadings: "#6e6e73"   // darker gray   
-        },
-        bacgroundGray: {
-            mainGray: "#154360",    
-            darkGray: "#f5f5f7"
-        },
-        customBlue: {
-            main: '#154360',        // blue
-            textColor: customGray.mainGray
-        },
-        customGreen: {
-            main: '#60ce80',        // green
-            textColor: customGray.mainGray
-        }
+  palette: {
+    textColor: {
+      main: '#1d1d15', // black
+      subMain: '#154360', // light gray
+      subHeadings: '#6e6e73', // darker gray
     },
-    typography: {
-        main: {
-            fontFamily: 'Source Sans Pro'
-        },
-        body: {
-            fontFamily: 'Quicksand'
-        }
-
-    }
+    bacgroundGray: {
+      mainGray: '#154360',
+      darkGray: '#f5f5f7',
+    },
+    customBlue: {
+      main: '#154360', // blue
+      textColor: customGray.mainGray,
+    },
+    customGreen: {
+      main: '#60ce80', // green
+      textColor: customGray.mainGray,
+    },
+  },
+  typography: {
+    main: {
+      fontFamily: 'Source Sans Pro',
+    },
+    body: {
+      fontFamily: 'Quicksand',
+    },
+  },
 })
