cda041e20134ccac230805b207b2f2c4083752b0
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-21 14:14:45 +0800

    feat: init forum actions

--- frontend/src/store/forum/actions.ts
+++ frontend/src/store/forum/actions.ts
@@ -6,7 +6,22 @@ const useForum = () => React.useContext<IForumState>(forumContext)
 export const retrieveForums = () => {
   const { forums } = useForum()
 
-  // TODO: filter forums by the current_accommodation
-
   return forums
+}
+
+export const setCurrentAccommodation = (id: string | undefined) => {
+  const { dispatch } = useForum()
+  dispatch({ type: 'FR_SET_CURRENT_AC', payload: id })
+}
+
+export const retrieveForumByCurrentAccommodation = () => {
+  const { forums /*current_accommodation*/ } = useForum()
+
+  // TODO: uncomment this one if ready to integrate with api
+  //   const filteredForums = forums.filter(
+  //     forum => forum.accommodation_id === current_accommodation
+  //   )
+
+  //   if (filteredForums.length === 0) return undefined
+  return forums[0]
 }
