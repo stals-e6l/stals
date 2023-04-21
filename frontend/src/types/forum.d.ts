512d4bf10a57aa86641bc48ea4326db604a424ef
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-21 13:32:27 +0800

    feat: add forum types

---
+++ frontend/src/types/forum.d.ts
@@ -0,0 +1,8 @@
+type TForumStatus = 'active' | 'archived' | 'deleted'
+
+interface IForum {
+  accommodation_id: string // the id of the accommodation where it belongs
+  is_public: boolean // whether the forum is available for public (unregistered) users or not
+  status: TForumStatus // the status of the forum, by default it is active
+  content: string[] // the array of forum content/comments
+}
