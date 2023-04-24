d90e650811e3444b7f6f8c7dc4911dbac31972f7
Josef Paul L. Gelera <jgelera@up.edu.ph>
2023-04-25 01:53:03 +0800

    "feat: added ui for update comment"

--- frontend/src/pages/Forum/UpdateCommentFromForum.tsx
+++ frontend/src/pages/Forum/UpdateCommentFromForum.tsx
@@ -1,12 +1,24 @@
-import { Button, TextField } from '@mui/material'
+import { Button, TextField, Dialog, Typography, Grid } from '@mui/material'
 import React from 'react'
 import { updateCommentFromForum } from '../../store/forum/actions'
+import { Height } from '@mui/icons-material'
 
 interface IProps {
   children?: React.ReactNode
   forumId: string
   comment: string
   commentIndex: number
+}
+
+const textFieldStyle = {
+  paddingRight: '10px',
+  paddingLeft: '10px',
+  height: '80%',
+  justifyContent: 'center'
+}
+
+const typographyStyle = {
+  padding: '10px',
 }
 
 const UpdateCommentFromForum: React.FC<IProps> = ({
@@ -17,21 +29,29 @@ const UpdateCommentFromForum: React.FC<IProps> = ({
   // hooks
   const updateCommentFromForumHandler = updateCommentFromForum()
   // state
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const [editable, setEditable] = React.useState<boolean>(false)
   const [comment, setComment] = React.useState<string>(initComment)
 
   return (
     <div>
-      {editable && (
+      {/* {editable && (
         <TextField
           value={comment}
           onChange={e => setComment(e.target.value)}
           placeholder="Enter comment..."
         />
-      )}
+      )} */}
       <Button
         variant="contained"
-        onClick={() => {
+        onClick={/*</div>*() => {
           // first it sets the UI to editable mode
           if (!editable) setEditable(true)
           // then when the user 'Saves'...
@@ -40,10 +60,47 @@ const UpdateCommentFromForum: React.FC<IProps> = ({
             updateCommentFromForumHandler(forumId, comment, commentIndex)
             setEditable(false)
           }
+        }*/
+        handleClickOpen
+      }
+      >
+        Update
+      </Button>
+
+      <Dialog fullWidth maxWidth='sm' open={open}
+        PaperProps={{
+          sx: {
+            //height: '40%',
+            borderRadius: '10px',
+          }
+        }}
+        sx={{
+          backdropFilter: 'blur(3px)',
         }}
       >
-        {!editable ? 'Update' : 'Save'}
-      </Button>
+        <Typography sx={typographyStyle}>Edit Comment</Typography>
+        <TextField
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+          placeholder="Enter comment..."
+          fullWidth
+          multiline
+          minRows={4}
+          maxRows={4}
+          sx={textFieldStyle}
+        />
+
+        <Grid container spacing={1} direction={'row'} justifyContent={'flex-end'} alignItems={'center'} sx={{minWidth:'100%', maxWidth:'100%', padding:'10px', margin:'auto',}}>
+          <Grid item xs={2} textAlign={'center'}>
+            <Button variant="contained" onClick={handleClose} sx={{width:'100%', maxWidth:'100%'}}>Cancel</Button>
+          </Grid>
+          <Grid item xs={2} textAlign={'center'}>
+            <Button variant="contained" onClick={handleClose} sx={{width:'100%', maxWidth:'100%'}}>
+              Confirm
+            </Button>
+          </Grid>
+        </Grid>
+      </Dialog>
     </div>
   )
 }
