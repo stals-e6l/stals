fbe6fbcbb3a1a4e25a86f9c3da9c3a51a3658adb
nesorinio <nesorinio@up.edu.ph>
2023-04-27 00:06:01 +0800

    refactor: improved error handling (retrieve-one-forum)

--- backend/api/forum.js
+++ backend/api/forum.js
@@ -104,26 +104,31 @@ forumRouter.get('/:id', async function(req, res){
         }
         const forum = await Forum.findById(req.params.id);
         if(!forum){
-            throw 404;
+            const error = new Error("Accomodation does not exist");
+            error.name = "NullError";
+            throw error;
         }
         res.status(200).json({success: true, data: forum});
     } catch(err){
-        switch(err) {
-            case 404:
-                res.status(404).json({ status: false, messages: ["Error: Forum could not be found"]});
+        let code;
+
+        switch (err.name) {
+            case "ValidationError":
+                code = 400;
                 break;
-            case 400:
-                res.status(400).json({ status: false, messages: ["Error: Bad request"]});
-              break;
-            case 401:
-                res.status(401).json({ status: false, messages: ["Error: Unauthorized access"]});
-              break;
-            case 500:
-                res.status(500).json({ status: false, messages: ["Error: Internal server error"]});
-              break;
+            case "CastError":
+                code = 400;
+                break;
+            case "AuthError":
+                code = 401;
+                break;
+            case "NullError":
+                code = 404;
+                break;
             default:
-                res.json({success: false, messages: [String(err)]});
+                code = 500;
         }
+        res.status(code).json({success: false, messages: [String(err)]});
     }
 })
 
