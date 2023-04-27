79dd68632f8cd2bb459520c1253b5e2adbcea886
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-27 14:02:47 +0800

    chore: remove createforum page

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -37,9 +37,4 @@ const router = createBrowserRouter([
     path: '/createaccommodation/',
     element: <CreateAccommodationPage />,
   },
-  {
-    path: '/addcomment/',
-    element: <AddCommentToForum />,
-  },
-
 ])
