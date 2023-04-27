e53fe4941f65eaded6ee0e6554679bf064d308cd
Benedict Sy <bsy@up.edu.ph>
2023-04-27 15:29:07 +0800

    refactor: updated report schema

--- backend/models/report.js
+++ backend/models/report.js
@@ -1,7 +1,17 @@
-const mongoose = require("mongoose")
+/* Updated schema */
+const mongoose = require("mongoose");
 
-const reportSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+const reportSchema = new mongoose.Schema({ 
+    timestamps: true, 
+    user_id: { // Sort of like a foreign key, user._id
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true 
+    },
+    pdf_url: { // Used to generate a pdf for the report
+        type: String, 
+        required: true
+    }, 
 });
 
 module.exports = mongoose.model("Report", reportSchema);
