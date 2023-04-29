55903d0947c1a741320bf4bc6581def6241ab835
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-04-29 21:55:31 +0800

    feat: init SignIn implementation

--- frontend/src/pages/Auth/SignIn.tsx
+++ frontend/src/pages/Auth/SignIn.tsx
@@ -5,7 +5,43 @@ interface IProps {
 }
 
 const SignIn: React.FC<IProps> = () => {
-  return <div>hello</div>
+  // states
+  const [form, setForm] = React.useState<IUserSignIn>({
+    userName: '',
+    passwordHash: '',
+  })
+
+  const handleSignIn = () => {
+    // TODO: handle sign in (PM's job)
+  }
+
+  return (
+    <div>
+      <div>
+        <label>Username</label>
+        <input
+          value={form.userName}
+          type="text"
+          placeholder="username"
+          onChange={e =>
+            setForm(prev => ({ ...prev, username: e.target.value }))
+          }
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          value={form.passwordHash}
+          type="password"
+          placeholder="password"
+          onChange={e =>
+            setForm(prev => ({ ...prev, passwordHash: e.target.value }))
+          }
+        />
+      </div>
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  )
 }
 
 export default SignIn
