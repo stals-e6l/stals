90df2bf06df362fffa6886da8021a40eebb2eaf2
jabarro <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>
2023-05-04 09:25:40 +0800

    feat: layout of accommodation results

--- frontend/src/pages/Accommodation/AccommodationResultsPage.tsx
+++ frontend/src/pages/Accommodation/AccommodationResultsPage.tsx
@@ -3,10 +3,13 @@ import {
   filterAccommodations,
   retrieveAccommodationResults,
 } from '../../store/accommodation/actions'
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { Button, Grid, Input, Typography, Slider, InputAdornment } from '@mui/material'
 import AccommodationTile from '../../components/accommTile'
 import { useSearchParams } from 'react-router-dom'
 import PreviewAccommodations from './PreviewAccommodations'
+import Header from '../../components/header'
+import SearchIcon from '@mui/icons-material/Search'
+import { useNavigate } from 'react-router-dom'
 
 interface IProps {
   children?: React.ReactNode
@@ -30,6 +33,12 @@ const AccomodationResultsPage: React.FC<IProps> = () => {
   })
   const [showPreview, setShowPreview] = React.useState<boolean>(false)
 
+  const navigate = useNavigate()
+  const [name, setName] = React.useState<string>('')
+  const handleSearch = () => {
+    navigate(`/accommodations/results?name=${name}`)
+  }
+
   // console.log(filter)
   const handleFilter = () => {
     filterAccommodationsHandler(filter)
@@ -39,46 +48,91 @@ const AccomodationResultsPage: React.FC<IProps> = () => {
     handleFilter()
   }, [])
 
-  return (
-    <Grid container>
-      {/* filters */}
-      <Grid
-        item
-        xs={3}
-        sx={{
-          background: 'orange',
-        }}
-      >
-        <Typography>Filters</Typography>
-        <TextField
+/*
+      <TextField
           type="number"
           onChange={e => {
             setFilter(prev => ({ ...prev, price: Number(e.target.value) }))
           }}
           value={filter.price}
         />
-        <Button onClick={handleFilter}>Filter it yow</Button>
-        <Button onClick={() => setShowPreview(true)}>Download</Button>
-      </Grid>
+*/
 
-      {/* results */}
-      <Grid item xs={9}>
-        {accommodationResults.map(accommodation => (
-          <AccommodationTile
-            key={accommodation._id}
-            accommodation={accommodation}
-          />
-        ))}
-      </Grid>
-
-      {showPreview && (
-        <PreviewAccommodations
-          show={showPreview}
-          onClose={() => setShowPreview(false)}
-          accommodations={accommodationResults}
-        />
-      )}
+  return (
+    <Grid container sx={{ flexDirection: 'column' }}>
+    <Grid item>
+        <Header />
     </Grid>
+    <Grid item xs={3}>
+        <Input
+            placeholder="Search Accommodation"
+            onChange={e => setName(e.target.value.trim())}
+            startAdornment={
+               <InputAdornment position="start" onClick={handleSearch}>
+                <SearchIcon />
+               </InputAdornment> 
+            }
+            disableUnderline
+            fullWidth
+            sx={{
+                p: 1,
+                m: 1,
+                border: '1px solid black',
+                borderRadius: '10px'
+            }}
+        ></Input>
+    </Grid>
+    <Grid item>
+    <Grid container sx={{ 
+        p: 1,
+        m: 'auto',
+        maxWidth: '1350px',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+    }}>
+          {/* filters */}
+          <Grid
+            item
+            xs={3}
+            sx={{
+              background: '#F0F0F0',
+              p: '15px',
+              borderRadius: '10px',
+              border: '1px grey solid',
+              height: '100%',
+              m: 'auto'
+            }}
+          >
+            <Typography>Filters</Typography>
+            <Button onClick={handleFilter}>Filter it yow</Button>
+            <Button onClick={() => setShowPreview(true)}>Download</Button>
+          </Grid>
+
+          {/* results */}
+          <Grid item xs={9} sx={{
+                p: 2,
+                m: 'auto',
+            }}>
+            {accommodationResults.map(accommodation => (
+              <AccommodationTile
+                key={accommodation._id}
+                accommodation={accommodation}
+              />
+            ))}
+          </Grid>
+
+          {showPreview && (
+            <PreviewAccommodations
+              show={showPreview}
+              onClose={() => setShowPreview(false)}
+              accommodations={accommodationResults}
+            />
+          )}
+        </Grid>
+    </Grid>
+</Grid>
+
   )
 }
 
