5c163d03f548169c4165e38c1a6edc26e42f0fa4
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-06 20:10:56 +0800

    faet: setup modules viewer

--- frontend/src/modules/index.tsx
+++ frontend/src/modules/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 interface IProps {
   children?: React.ReactNode
 }
 
 const ModulesViewer: React.FC<IProps> = () => {
+  const navigate = useNavigate()
+  const modules = [
+    '/AccommodationCard',
+    '/AccommodationForm',
+    '/AccommodationFormModal',
+    '/AccommodationResults',
+    '/DeleteAccommodationForm',
+    '/DeleteAccommodationFormModal',
+    '/DownloadAccommodations',
+    '/FilterAccommodations',
+    '/SearchAccommodations',
+    '/SignInForm',
+    '/SignUpForm',
+  ]
   return (
     <div>
-      safsf
+      <ul
+        style={{
+          marginBottom: '20px',
+        }}
+      >
+        {modules &&
+          modules.map(module => (
+            <li
+              style={{ textDecoration: 'underline', cursor: 'pointer' }}
+              key={module}
+              onClick={() => navigate(module)}
+            >
+              {module}
+            </li>
+          ))}
+      </ul>
+      <hr style={{ marginBottom: '20px' }} />
       <Outlet />
     </div>
   )
