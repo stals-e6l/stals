66727e797fb826208903ba37cef5c71efa8de417
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-06 20:26:21 +0800

    feat: setup AccommodationForm & modal

--- frontend/src/modules/accommodation/AccommodationForm.tsx
+++ frontend/src/modules/accommodation/AccommodationForm.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 interface IProps {
   children?: React.ReactNode
+  defaultValues?: IAccommodation
 }
 
-const AccommodationForm: React.FC<IProps> = () => {
-  return <div>hello</div>
+const AccommodationForm: React.FC<IProps> = ({ defaultValues }) => {
+  return <div>{JSON.stringify(defaultValues)}</div>
 }
 
 export default AccommodationForm
--- frontend/src/modules/accommodation/AccommodationFormModal.tsx
+++ frontend/src/modules/accommodation/AccommodationFormModal.tsx
@@ -1,11 +1,51 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@mui/material'
 import React from 'react'
+import AccommodationForm from './AccommodationForm'
 
 interface IProps {
   children?: React.ReactNode
+  defaultValues?: IAccommodation
 }
 
-const AccommodationFormModal: React.FC<IProps> = () => {
-  return <div>hello</div>
+const AccommodationFormModal: React.FC<IProps> = ({ defaultValues }) => {
+  // state
+  const [open, setOpen] = React.useState<boolean>(false)
+
+  // events
+  const toggleDialog = () => setOpen(prev => !prev)
+  const handleSubmit = () => {
+    // TODO: PM's job
+  }
+
+  return (
+    <React.Fragment>
+      <Button onClick={toggleDialog}>
+        {defaultValues ? 'Update accommodation' : 'Create accommodation'}
+      </Button>
+      {open && (
+        <Dialog open={open} onClose={toggleDialog}>
+          <DialogTitle>
+            {defaultValues ? 'Update accommodation' : 'Create accommodation'}
+          </DialogTitle>
+
+          <DialogContent>
+            <AccommodationForm defaultValues={defaultValues} />
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={toggleDialog}>Cancel</Button>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </React.Fragment>
+  )
 }
 
 export default AccommodationFormModal
