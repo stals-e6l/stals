5aaed2ba50faeeb20aaeb4fcaf940a8caa8fc7bb
rbsunga2020 <rbsunga@up.edu.ph>
2023-05-09 19:15:08 +0800

    feat: added error handling for the auth middleware

--- backend/handler/auth_middleware.js
+++ backend/handler/auth_middleware.js
@@ -1,5 +1,9 @@
 const jwt = require("jsonwebtoken");
 
+const { ERRORS } = require("./error_handler")
+const { ErrorHandler } = require("./error_handler")
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const authMid = router();
 
 authMid('*', async(req, res, next) => {
@@ -7,23 +11,39 @@ authMid('*', async(req, res, next) => {
         const authHeader = req.headers.authorization
     
         if (!authHeader) {
+            var error = new Error();
+            error.name = "AuthError";
+            throw error;
         }
     
         const [authMethod, token] = authHeader.split(' ')
     
         if (authMethod !== 'Bearer') {
+            var error = new Error();
+            error.name = "AuthError";
+            throw error;
         }
     
         if (!token) {
+            var error = new Error();
+            error.name = "AuthError";
+            throw error;
         }
     
         const decoded = jwt.verify(token, PRIVATE_KEY)
         const dbUser = await User.findById(decoded.id)
     
         if (!dbUser) {
+            var error = new Error();
+            error.name = "AuthError";
+            throw error;
         }
-    
+
+        next();
+        
     } catch (err) {
-    
+        let code = ErrorHandler(err.name);
+
+        res.status(code).json({success: false, messages: [ERRORS[code]]});
     }
 })
