34d0bda80f6ab9ad855918d88e27263484c5d64f
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-10 19:36:04 +0800

    fix: uninit var in error handler

--- backend/handler/error_handler.js
+++ backend/handler/error_handler.js
@@ -1,31 +1,32 @@
 const ERRORS = {
-    200: "Success",
-    201: "Creation success",
-    400: "Bad request",
-    401: "Unauthorized access",
-    404: "Not found",
-    500: "Internal server error"
+  200: 'Success',
+  201: 'Creation success',
+  400: 'Bad request',
+  401: 'Unauthorized access',
+  404: 'Not found',
+  500: 'Internal server error',
 }
 
-const ErrorHandler = function(err_name){
-    switch (err_name) {
-        case "ValidationError":
-            code = 400;
-            break;
-        case "CastError":
-            code = 400;
-            break;
-        case "AuthError":
-            code = 401;
-            break;
-        case "NullError":
-            code = 404;
-            break;
-        default:
-            code = 500;
-    }
+const ErrorHandler = function (err_name) {
+  let code
+  switch (err_name) {
+    case 'ValidationError':
+      code = 400
+      break
+    case 'CastError':
+      code = 400
+      break
+    case 'AuthError':
+      code = 401
+      break
+    case 'NullError':
+      code = 404
+      break
+    default:
+      code = 500
+  }
 
-    return code;
+  return code
 }
 
-module.exports = { ErrorHandler, ERRORS };
+module.exports = { ErrorHandler, ERRORS }
