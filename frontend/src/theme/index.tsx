9319c05cf661723916ec1cb7fe3d47fb676abe6b
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-10 20:03:41 +0800

    feat: add Color

--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -1,16 +1,26 @@
-// const theme = createTheme({
+// types
+type TColor =
+  | 'gray1'
+  | 'gray2'
+  | 'black'
+  | 'blue'
+  | 'green'
+  | 'textBlack'
+  | 'textGray'
 
-//   });
-
-export const colors = {
-  blue: '#154360',
-  green: '#60ce80',
-  grey: '#f0f0f0',
-  darkGrey: '#f5f5f7',
-  quicksand: 'Quicksand',
+type IColor = {
+  [key in TColor]: string
 }
 
-export const fontFamily = 'Source Sans Pro'
+export const COLOR: IColor = {
+  gray1: '#f5f5f7',
+  gray2: '#f0f0f0',
+  black: '#1d1d15',
+  blue: '#154360',
+  green: '#60ce80',
+  textBlack: '#1d1d1f',
+  textGray: '#6e6e73',
+}
 
 const theme = {}
 export default theme
