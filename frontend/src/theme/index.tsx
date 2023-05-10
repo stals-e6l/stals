b0039b4c7d4e8a85cd1ae1eea5d858f7cc110c81
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-10 20:09:43 +0800

    feat: add Font and types

--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -12,6 +12,11 @@ type IColor = {
   [key in TColor]: string
 }
 
+type TFont = 'sourceSansPro' | 'quicksand'
+type IFont = {
+  [key in TFont]: string
+}
+
 export const COLOR: IColor = {
   gray1: '#f5f5f7',
   gray2: '#f0f0f0',
@@ -20,6 +25,11 @@ export const COLOR: IColor = {
   green: '#60ce80',
   textBlack: '#1d1d1f',
   textGray: '#6e6e73',
+}
+
+export const FONT: IFont = {
+  sourceSansPro: 'Source Sans Pro',
+  quicksand: 'Quicksand',
 }
 
 const theme = {}
