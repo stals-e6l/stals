78f2fd27c1e94acfb375879924ec79d1841523c3
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-12 03:06:37 +0800

    refactor: review schema

--- database/nosql/review.js
+++ database/nosql/review.js
@@ -1,42 +1,26 @@
 const mongoose = require("mongoose")
 
-const reviewSchema = new mongoose.Schema({
-    rating: {
-        type: Number,
-        required: true,
-    },
-    comment: {
-        type: String,
-    },
-    // content: { // array ng comments/chats ng isang user
-    //     type: [String],
-    //     required: true,
-    //     default: [],
-    // },
-    // status: { // status of the forum/chat conversation
-    //     type: String,
-    //     enum: ['active', 'archived', 'deleted'],
-    //     required: true
-    // },
-    is_public: { // either public forum or private chat
-        type: Boolean,
-        required: true
-    },
-    accommodation_id: { // accomodation reference
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Accommodation',
-        required: true
-    },
-    user_id: { // user reference
-         type: mongoose.Schema.Types.ObjectId,
-         ref: 'User',
-         required: true
-    },
-    is_soft_deleted: { 
-        type: Boolean, 
-        default: false, 
-        required: true 
-    },
+const reviewSchema = new mongoose.Schema(
+    {
+        rating: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 5
+        },
+        comment: {
+            type: String,
+        },
+        accommodation_id: { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Accommodation',
+            required: true
+        },
+        user_id: { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
     },
     { timestamps: true },
 );
