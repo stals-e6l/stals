94a508e6df1644ba927ffd9971314cb55e19580e
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-13 01:03:36 +0800

    chore: cleanup AppRouter

--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import AccommodationPage from './pages/Accommodation/AccommodationPage'
-// import AccommodationDetailPage from './pages/Accommodation/AccommodationDetailPage'
-// import SamplePage from './pages/Sample'
-// import CreateAccommodationPage from './pages/CreateAccommodation'
-// import AccomodationResultsPage from './pages/Accommodation/AccommodationResultsPage'
-// import SignUp from './pages/Auth/SignUp'
-// import SignIn from './pages/Auth/SignIn'
-// import Profile from './pages/Auth/Profile'
-// import AuthWrapper from './pages/Auth'
 import AccommodationCard from '../modules/accommodation/AccommodationCard'
 import AccommodationForm from '../modules/accommodation/AccommodationForm'
 import AccommodationFormModal from '../modules/accommodation/AccommodationFormModal'
@@ -39,7 +30,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/AccommodationCard',
-        element: <AccommodationCard />,
+        element: <AccommodationCard accommodation={{} as any} />,
       },
       {
         path: '/AccommodationForm',
@@ -59,7 +50,12 @@ const router = createBrowserRouter([
       },
       {
         path: '/DeleteAccommodationFormModal',
-        element: <DeleteAccommodationFormModal />,
+        element: (
+          <DeleteAccommodationFormModal
+            accommodationId={''}
+            isSoftDelete={false}
+          />
+        ),
       },
       {
         path: '/DownloadAccommodations',
@@ -83,38 +79,4 @@ const router = createBrowserRouter([
       },
     ],
   },
-  // {
-  //   path: '/',
-  //   element: <AuthWrapper />,
-  //   children: [
-  //     {
-  //       path: '/accommodations',
-  //       element: <AccommodationPage />,
-  //     },
-  //     {
-  //       path: '/accommodations/:id',
-  //       element: <AccommodationDetailPage />,
-  //     },
-  //     {
-  //       path: '/createaccommodation/',
-  //       element: <CreateAccommodationPage />,
-  //     },
-  //     {
-  //       path: '/accommodations/results',
-  //       element: <AccomodationResultsPage />,
-  //     },
-  //     {
-  //       path: '/sign-in',
-  //       element: <SignIn />,
-  //     },
-  //     {
-  //       path: '/profile',
-  //       element: <Profile />,
-  //     },
-  //   ],
-  // },
-  // {
-  //   path: '/sample',
-  //   element: <SamplePage />,
-  // },
 ])
