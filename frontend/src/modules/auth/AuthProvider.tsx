b77ccc59a0f64f5ed30300c0293909d508ae9b64
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-13 17:01:46 +0800

    feat: setup AuthProvider

--- frontend/src/modules/auth/AuthProvider.tsx
+++ frontend/src/modules/auth/AuthProvider.tsx
@@ -5,7 +5,44 @@ interface IProps {
 }
 
 const AuthProvider: React.FC<IProps> = ({ children }) => {
-  return <div>{children}</div>
+  //state
+  const [state, dispatch] = React.useReducer(authReducer, {
+    user: null,
+    dispatch: null,
+  })
+
+  return (
+    <authContext.Provider
+      value={{
+        user: state.user,
+        dispatch,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  )
 }
 
 export default AuthProvider
+
+const authContext = React.createContext<IAuthState>({
+  user: null,
+  dispatch: null,
+})
+
+export const useAuth = () => React.useContext<IAuthState>(authContext)
+
+const authReducer = (
+  state: IAuthState,
+  action: IReducerAction<TAuthActionType, TAuthActionPayload>
+): IAuthState => {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.payload as any,
+      }
+    default:
+      return state
+  }
+}
