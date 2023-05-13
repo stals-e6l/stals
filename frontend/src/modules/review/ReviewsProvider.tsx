26cc437f7456bdb300ce50dcfdce722db06e28b3
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-13 15:38:12 +0800

    feat: setup reviews provider

--- frontend/src/modules/review/ReviewsProvider.tsx
+++ frontend/src/modules/review/ReviewsProvider.tsx
@@ -5,7 +5,42 @@ interface IProps {
 }
 
 const ReviewsProvider: React.FC<IProps> = ({ children }) => {
-  return <div>{children}</div>
+  //state
+  const [state, dispatch] = React.useReducer(reviewsReducer, {
+    reviews: null,
+    dispatch: null,
+  })
+
+  return (
+    <reviewsContext.Provider
+      value={{
+        reviews: state.reviews,
+        dispatch,
+      }}
+    >
+      {children}
+    </reviewsContext.Provider>
+  )
 }
 
 export default ReviewsProvider
+
+const reviewsContext = React.createContext<IReviewsState>({
+  reviews: null,
+  dispatch: null,
+})
+
+const reviewsReducer = (
+  state: IReviewsState,
+  action: IReducerAction<TReviewActionType, TReviewActionPayload>
+): IReviewsState => {
+  switch (action.type) {
+    case 'SET_REVIEWS':
+      return {
+        ...state,
+        reviews: action.payload as IReview[],
+      }
+    default:
+      return state
+  }
+}
