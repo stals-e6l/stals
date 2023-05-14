270717e108b44d9438e8f2bfb251f3284ec56f3e
ThisIsBONA <mgbonayon1@up.edu.ph>
2023-05-14 23:05:35 +0800

    refactor: updated model reference for auth in api

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const User = require('../models/v2/user')
 
 const authRouter = Router()
 const saltRounds = 10
