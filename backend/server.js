34c3a7350cd38b98ca8fabbca0b3e493f8fde73b
ThisIsBONA <mgbonayon1@up.edu.ph>
2023-05-14 21:54:55 +0800

    refactor: updated api reference in server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ server.use(bodyParser.json());
 
 /** INSERT API BELOW */
 server.use('/api/ping', require('./api/ping'))
-server.use('/api', require('./api/accommodation'))
+server.use('/api/accommodation', require('./api/accommodation'))
 server.use('/api/review', require('./api/review'))
 server.use('/api/report', require('./api/report'))
 server.use('/api', require('./api/auth'))
