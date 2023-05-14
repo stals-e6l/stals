51d29f5a17391d70452979206b6ca10f2bb415ef
ThisIsBONA <mgbonayon1@up.edu.ph>
2023-05-14 23:06:47 +0800

    feat: added some validations for accommodation schema

--- backend/models/v2/accommodation.js
+++ backend/models/v2/accommodation.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+const User = require('./user')
+
 const accommodationTypeEnum = [
   'hotel',
   'apartment',
@@ -15,50 +17,88 @@ const accommodationFurnishingEnum = [
 
 const accommodationSchema = new mongoose.Schema(
   {
-    user_id: { type: mongoose.Schema.ObjectId, ref: 'User', required: false },
+    user_id: { type: mongoose.Schema.ObjectId, 
+      ref: 'User', 
+      validate: {
+        validator: async function(value){
+          const user = await User.findById(value);
+          if (!user) return false;
+          else return true;
+        }, message: "The user accessing the accommodation does not exist"
+      },
+      required: false 
+    },
     description: { type: String, required: false },
-    name: { type: String, required: true },
+    name: { type: String, required: [true, "Accommodation name is required."]},
     image: {
       url: {
-        type: String,
+        type: String, required: false
       },
-      // TODO: sprint 5
-      // filename:  {   // uses Multer
-      //   type: String,
-      // }F
-      // desc:  {
-      //   type: String,
-      // },
-      // file:
-      // {
-      //   data: {
-      //     type: Buffer,
-      //   },
-      //   contentType: {
-      //     type: String
-      //   }
-      // },
     },
-    address: { type: String, required: true },
+    address: { type: String, required: [true, "Accommodation address is required."]},
     type: {
       type: String,
-      required: true,
+      validate: {
+        validator: function(value){
+          return accommodationTypeEnum.includes(value);
+        }, message: props => `${props.value} is not a valid type.`
+      },
+      required: [true, "Accommodation type is required."],
       enum: accommodationTypeEnum,
     },
     furnishing: {
       type: String,
-      required: true,
+      validate: {
+        validator: function(value){
+          return accommodationFurnishingEnum.includes(value);
+        }, message: props => `${props.value} is not a furnishing description.`
+      },
+      required: [true, "Accommodation furnishing description is required"],
       enum: accommodationFurnishingEnum,
     },
-    min_price: { type: Number, required: true, min: 0 },
-    max_price: { type: Number, required: true },
-    size_sqm: { type: Number, required: true, min: 0 },
-    meters_from_uplb: { type: Number, required: true, min: 0 },
-    min_pax: { type: Number, required: true, min: 0 },
+    min_price: { 
+      type: Number, 
+      validate: {
+        validator: function(value){
+          if (value < 0)  return false;
+          else return true;
+        }
+      },
+      required: [true, "Accommodation minimum price is required."], 
+    },
+    max_price: { type: Number, required: [true, "Accommodation maximum price is required."] },
+    size_sqm: { type: Number, validate:{
+      validator: function(value){
+        if(value < 0) return false;
+        else return true;
+      }
+     }, required: [true, "Accommodation size in square meter is required."]},
+    meters_from_uplb: { type: Number, required: [true, "Accommodation distance from UPLB is required."]},
+    min_pax: { type: Number, validate:{
+      validator: function(value){
+        if(value < 0) return false;
+        else return true;
+      }, message: "Minimum number of pax allowed should be 0."
+     }, required: true},
     max_pax: { type: Number, required: true },
-    num_rooms: { type: Number, required: true, min: 0 },
-    num_beds: { type: Number, required: true, min: 0 },
-    num_views: { type: Number, required: true, min: 0 },
+    num_rooms: { type: Number, validate:{
+      validator: function(value){
+        if(value < 0) return false;
+        else return true;
+      }, message: "Minimum number of rooms allowed should be 0."
+     }, required: true},
+    num_beds: { type: Number, validate:{
+      validator: function(value){
+        if(value < 0) return false;
+        else return true;
+      }, message: "Minimum number of beds allowed should be 0."
+     }, required: true},
+    num_views: { type: Number, validate:{
+      validator: function(value){
+        if(value < 0) return false;
+        else return true;
+      }, message: "Minimum number of views allowed should be 0."
+     }, required: true},
     landmarks: { type: [String], required: true, default: [] },
     cooking_rules: { type: [String], required: true, default: [] },
     pet_rules: { type: [String], required: true, default: [] },
@@ -68,7 +108,6 @@ const accommodationSchema = new mongoose.Schema(
     amenities: { type: [String], required: true, default: [] },
     is_soft_deleted: { type: Boolean, default: false, required: true },
   },
-  { timestamps: true }
-)
+  { timestamps: true })
 
 module.exports = mongoose.model('Accommodation', accommodationSchema)
