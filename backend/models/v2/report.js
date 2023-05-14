d43526d4449b566972f523762feec6593d5f065d
nesorinio <100125557+nesorinio@users.noreply.github.com>
2023-05-15 01:56:36 +0800

    feat: added report model validator (user_id and pdf_url)

---
+++ backend/models/v2/report.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const User = require('./user')
+
+const reportSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      validate: {
+        validator: async function(id) {
+          const user = await User.findById(id);
+          if (!user) return false;
+          else return true;
+        },
+        message: "User id for the report not found"
+      },
+      required: [true, 'User id is required'],
+    },
+    pdf_url: {
+      type: String,
+      validate: {
+        validator: async function(url) {
+          // url_regex will accept the following examples:
+          //    -- facebook.com
+          //    -- https://facebook.com
+          //    -- https://www.facebook.com
+          const url_regex = new RegExp('((https?:\/\/(?:www\.|(?!www)))?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
+
+          if (!url_regex.test(url)) return false;
+          else return true;
+        },
+        message: "The provided URL for the report is not valid"
+      },
+      required: [true, 'PDF URL is required'],
+      // TODO: sprint 5 (upload pdf file)
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Report', reportSchema)
