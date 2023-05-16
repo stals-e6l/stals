b9e5dd819632243de56f7db2ba5d0a9fa2470d6f
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-17 00:20:11 +0800

    merge

--- frontend/src/theme/AppTheme.tsx
+++ frontend/src/theme/AppTheme.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from '.'
-import { AdapterDayjs } from '@mui/lab'
-import { LocalizationProvider } from '@mui/lab'
 
 interface IProps {
   children?: React.ReactNode
 }
-
-function App({ children }) {
-    return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        {children}
-      </LocalizationProvider>
-    )
-  }
 
 const AppTheme: React.FC<IProps> = ({ children }) => {
   return (
