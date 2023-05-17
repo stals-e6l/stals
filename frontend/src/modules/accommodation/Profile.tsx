71925423b4fd3e5b053d3f6bba7f6bbdae9829cd
rodgedeluna <rmdeluna@up.edu.ph>
2023-05-17 22:48:27 +0800

    feat: create and display hardcoded data

--- frontend/src/modules/accommodation/Profile.tsx
+++ frontend/src/modules/accommodation/Profile.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import Header from '../../components/header'
-import { Box, Typography, IconButton, useTheme } from '@mui/material'
+import {
+  Box,
+  Typography,
+  IconButton,
+  useTheme,
+  Input,
+  Button,
+  Avatar,
+} from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { getUser } from '../../store/auth/action'
+import { COLOR } from '../../theme'
+import location from '../../assets/Images/Ellens.jpg'
 
 interface IProps {
   children?: React.ReactNode
 }
 
 const Profile: React.FC<IProps> = () => {
-//   const user = getUser()
+  //   const user = getUser()
 
-//   if (!user) {
-//     return <div>no user</div>
-//   }
-const theme = useTheme()
+  //   if (!user) {
+  //     return <div>no user</div>
+  //   }
+  const theme = useTheme()
+  const [image, setImage] = React.useState<any>(null)
+
+  const user = {
+    username: 'Username',
+    email: 'test@up.edu.ph',
+    role: 'Admin',
+    bio: 'Test bio',
+  }
 
   return (
     <React.Fragment>
@@ -23,7 +41,7 @@ const theme = useTheme()
       <Box sx={{ display: 'flex' }}>
         <Box
           sx={{
-            backgroundColor: '#89CFF0',
+            backgroundColor: theme.palette.primary.main,
             width: '30%',
             height: '100vh',
             padding: theme.spacing(5),
@@ -31,14 +49,41 @@ const theme = useTheme()
         >
           <Box sx={{ display: 'flex' }}>
             <IconButton>
-              <ArrowBackIcon sx={{ fontSize: '5vh' }} />
+              <ArrowBackIcon sx={{ fontSize: '5vh', color: COLOR.white }} />
             </IconButton>
-            <AccountCircleIcon sx={{ fontSize: '10vh' }} />
+            <Avatar
+              sx={{
+                width: theme.spacing(6),
+                height: theme.spacing(6),
+              }}    
+              alt="profile"
+              src={image}
+            />
           </Box>
-          <Typography variant="h3">Greetings,</Typography>
-          <Typography variant="h3">Pogi</Typography>
-          <Typography variant="h5">test</Typography>
-          <Typography variant="h5">test </Typography>
+          <Typography variant="h4" sx={{ color: COLOR.white }}>
+            Greetings,
+          </Typography>
+          <Typography variant="h5" sx={{ color: COLOR.white }}>
+            {user.username}
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: 'light', color: COLOR.white }}
+          >
+            {user.email}
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: 'light', color: COLOR.white }}
+          >
+            {user.role}{' '}
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: 'light', color: COLOR.white }}
+          >
+            {user.bio}{' '}
+          </Typography>
         </Box>
       </Box>
     </React.Fragment>
