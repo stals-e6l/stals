4354453ea9496ce6bc843998f7fe1cc02a210c31
rodgedeluna <rmdeluna@up.edu.ph>
2023-05-18 17:54:03 +0800

    feat: dialog for edit page

--- frontend/src/modules/accommodation/Profile.tsx
+++ frontend/src/modules/accommodation/Profile.tsx
@@ -8,15 +8,126 @@ import {
   Input,
   Button,
   Avatar,
+  Dialog,
+  TextField,
+  Link,
+  Card,
+  CardActionArea,
+  CardContent,
+  FormLabel,
 } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import PhotoCamera from '@mui/icons-material/PhotoCamera'
+
 import { getUser } from '../../store/auth/action'
 import { COLOR } from '../../theme'
 import location from '../../assets/Images/Ellens.jpg'
+import { Edit } from '@mui/icons-material'
 
 interface IProps {
   children?: React.ReactNode
+}
+
+const EditProfile: React.FC<IProps> = () => {
+  const theme = useTheme()
+  const handleUploadImage = () => {
+    // execute code to open file input
+    const fileInput = document.querySelector(
+      'input[type=file]'
+    ) as HTMLInputElement
+    if (fileInput) {
+      fileInput.click()
+    }
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        backdropFilter: 'blur(5px)',
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: COLOR.gray2,
+          width: '100%',
+          height: '100%',
+          padding: theme.spacing(2),
+          borderRadius: theme.spacing(1),
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: 'bold',
+            color: theme.palette.primary.main,
+            paddingBottom: theme.spacing(1),
+          }}
+        >
+          Edit Profile
+        </Typography>
+
+        
+        <FormLabel>Upload Avatar</FormLabel>
+        <Card
+          sx={{
+            minWidth: '100%',
+            minHeight: '50%',
+            border: 'solid 1px green',
+            backgroundColor: COLOR.white,
+          }}
+          onClick={handleUploadImage}
+        >
+          <CardActionArea>
+            <CardContent
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                maxWidth: '100%',
+                paddingBottom: '20%',
+                paddingTop: '20%',
+              }}
+            >
+              <PhotoCamera />
+            </CardContent>
+          </CardActionArea>
+          <input hidden accept="image/*" type="file" />
+        </Card>
+        
+        <FormLabel>Biography</FormLabel>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+          //   value={form.email}
+          //   onChange={e => setForm(prev => ({ ...prev, email: e.target.value }))}
+        />
+
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+            marginTop: theme.spacing(2),
+            marginBottom: theme.spacing(1),
+          }}
+          //   disabled={
+          //     form.password.length === 0 || form.password !== form.confirm
+          //   }
+          //   onClick={handleSignUp}
+        >
+          Save Changes
+        </Button>
+      </Box>
+    </Box>
+  )
 }
 
 const Profile: React.FC<IProps> = () => {
@@ -27,6 +138,10 @@ const Profile: React.FC<IProps> = () => {
   //   }
   const theme = useTheme()
   const [image, setImage] = React.useState<any>(null)
+
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   const user = {
     username: 'Username',
@@ -55,15 +170,15 @@ const Profile: React.FC<IProps> = () => {
               sx={{
                 width: theme.spacing(6),
                 height: theme.spacing(6),
-              }}    
+              }}
               alt="profile"
               src={image}
             />
           </Box>
-          <Typography variant="h4" sx={{ color: COLOR.white }}>
+          <Typography variant="h3" sx={{ color: COLOR.white}}>
             Greetings,
           </Typography>
-          <Typography variant="h5" sx={{ color: COLOR.white }}>
+          <Typography variant="h4" sx={{ color: COLOR.white , fontWeight:'light'}}>
             {user.username}
           </Typography>
           <Typography
@@ -84,6 +199,20 @@ const Profile: React.FC<IProps> = () => {
           >
             {user.bio}{' '}
           </Typography>
+          <Button onClick={handleOpen}>Edit Profile</Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            sx={{
+              width: '30%',
+              margin: '0 auto',
+              [theme.breakpoints.down('sm')]: {
+                width: '100%',
+              },
+            }}
+          >
+            <EditProfile />
+          </Dialog>
         </Box>
       </Box>
     </React.Fragment>
