6b800a7676842b47aebc3b194fbf655564b14262
sallysanban <johanna.eikou@gmail.com>
2023-05-21 01:02:12 +0800

    fix: removed console.log

--- backend/handler/rest_router.js
+++ backend/handler/rest_router.js
@@ -44,11 +44,9 @@ const RESTRouter = function (name, model, restriction) {
 
       let query = { ...req.query }
       delete query['limit'] //delete every query that's not part of the database model
-      //delete query['search']
 
       const limit = Number(req.query.limit) || 100
 
-      console.log({ search: query.search })
       if (!query.search) {
         data = await model.find(query).limit(limit)
       } else {
