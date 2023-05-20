abbc9f97f76330c345452dba5faa39623ebf1a27
rodgedeluna <rmdeluna@up.edu.ph>
2023-05-21 00:24:08 +0800

    refactor: extracted editprofile

--- frontend/src/modules/auth/EditProfile.tsx
+++ frontend/src/modules/auth/EditProfile.tsx
@@ -2,121 +2,224 @@ import React from 'react'
 import {
   Box,
   Typography,
-
+  IconButton,
   useTheme,
+  Input,
   Button,
+  Avatar,
+  Dialog,
   TextField,
+  Link,
   Card,
   CardActionArea,
   CardContent,
   FormLabel,
+  Divider,
+  Grid,
+  MenuItem,
+  Select,
+  InputLabel,
 } from '@mui/material'
 import PhotoCamera from '@mui/icons-material/PhotoCamera'
-import { COLOR } from '../../theme'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { COLOR } from '../../theme/index'
 
 interface IProps {
-
+  children?: React.ReactNode
 }
 
 const EditProfile: React.FC<IProps> = () => {
-    const theme = useTheme()
-    const handleUploadImage = () => {
-      // execute code to open file input
-      const fileInput = document.querySelector(
-        'input[type=file]'
-      ) as HTMLInputElement
-      if (fileInput) {
-        fileInput.click()
-      }
+  const theme = useTheme()
+  const handleUploadImage = () => {
+    // execute code to open file input
+    const fileInput = document.querySelector(
+      'input[type=file]'
+    ) as HTMLInputElement
+    if (fileInput) {
+      fileInput.click()
     }
-  
-    return (
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          backdropFilter: 'blur(5px)',
-        }}
-      >
-        <Box
-          sx={{
-            backgroundColor: COLOR.gray2,
-            width: '100%',
-            height: '100%',
-            padding: theme.spacing(2),
-            borderRadius: theme.spacing(1),
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{
-              fontWeight: 'bold',
-              color: theme.palette.primary.main,
-              paddingBottom: theme.spacing(1),
-            }}
-          >
-            Edit Profile
-          </Typography>
-  
-          
-          <FormLabel>Upload Avatar</FormLabel>
-          <Card
-            sx={{
-              minWidth: '100%',
-              minHeight: '50%',
-              border: 'solid 1px green',
-              backgroundColor: COLOR.white,
-            }}
-            onClick={handleUploadImage}
-          >
-            <CardActionArea>
-              <CardContent
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  maxWidth: '100%',
-                  paddingBottom: '20%',
-                  paddingTop: '20%',
-                }}
-              >
-                <PhotoCamera />
-              </CardContent>
-            </CardActionArea>
-            <input hidden accept="image/*" type="file" />
-          </Card>
-          
-          <FormLabel>Biography</FormLabel>
-          <TextField
-            id="outlined-basic"
-            variant="outlined"
-            size="small"
-            fullWidth
-            sx={{ backgroundColor: COLOR.white }}
-            //   value={form.email}
-            //   onChange={e => setForm(prev => ({ ...prev, email: e.target.value }))}
-          />
-  
-          <Button
-            variant="contained"
-            fullWidth
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              marginTop: theme.spacing(2),
-              marginBottom: theme.spacing(1),
-            }}
-            //   disabled={
-            //     form.password.length === 0 || form.password !== form.confirm
-            //   }
-            //   onClick={handleSignUp}
-          >
-            Save Changes
-          </Button>
-        </Box>
-      </Box>
-    )
   }
 
-  export default EditProfile
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        pb: theme.spacing(1),
+        alignItems: 'center',
+        width: '100%',
+        backdropFilter: 'blur(5px)',
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: COLOR.gray2,
+          width: '100%',
+          height: '100%',
+          padding: theme.spacing(2),
+          borderRadius: theme.spacing(1),
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: 'bold',
+            color: theme.palette.primary.main,
+            paddingBottom: theme.spacing(1),
+          }}
+        >
+          Edit Profile
+        </Typography>
+
+        <FormLabel>Upload Avatar</FormLabel>
+        <Card
+          sx={{
+            minWidth: '100%',
+            minHeight: '50%',
+            border: 'solid 1px green',
+            backgroundColor: COLOR.white,
+          }}
+          onClick={handleUploadImage}
+        >
+          <CardActionArea>
+            <CardContent
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                pb: theme.spacing(1),
+                alignItems: 'center',
+                maxWidth: '100%',
+                paddingBottom: '20%',
+                paddingTop: '20%',
+              }}
+            >
+              <PhotoCamera />
+            </CardContent>
+          </CardActionArea>
+          <input hidden accept="image/png, image/jpeg" type="file" />
+        </Card>
+
+        <FormLabel>Username</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <FormLabel>Biography</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <Grid container spacing={1}>
+          <Grid item xs={7}>
+            <FormLabel>Gender</FormLabel>
+            <Select
+              label=""
+              fullWidth
+              sx={{ backgroundColor: COLOR.white }}
+              size="small"
+            >
+              <MenuItem value={'male'}>Male</MenuItem>
+              <MenuItem value={'female'}>Female</MenuItem>
+              <MenuItem value={'non_binary'}>Non-Binary</MenuItem>
+              <MenuItem value={'prefer_not_to_say'}>Prefer not to say</MenuItem>
+            </Select>
+          </Grid>
+
+          <Grid item xs={5}>
+            <InputLabel>Birthday</InputLabel>
+            <DatePicker
+              // value={form.birthday as string}
+              slotProps={{ textField: { size: 'small' } }}
+            />
+          </Grid>
+        </Grid>
+
+        <FormLabel>Email</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <FormLabel>Phone</FormLabel>
+            <TextField
+              variant="outlined"
+              size="small"
+              fullWidth
+              sx={{ backgroundColor: COLOR.white }}
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <FormLabel>Landline</FormLabel>
+            <TextField
+              variant="outlined"
+              size="small"
+              fullWidth
+              sx={{ backgroundColor: COLOR.white }}
+            />
+          </Grid>
+        </Grid>
+
+        <FormLabel>Home Address</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <FormLabel>Current Address</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <FormLabel>Organization</FormLabel>
+        <TextField
+          variant="outlined"
+          size="small"
+          fullWidth
+          sx={{ backgroundColor: COLOR.white }}
+        />
+
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+            marginTop: theme.spacing(2),
+            marginBottom: theme.spacing(1),
+          }}
+        >
+          Cancel
+        </Button>
+
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+            marginTop: theme.spacing(2),
+            marginBottom: theme.spacing(1),
+          }}
+        >
+          Save Changes
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default EditProfile
