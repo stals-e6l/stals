9765bee930e25373c36b5d67748d5868fca9f680
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-05-24 01:51:51 +0800

    fix: cors, endpoints issue

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import { getToken } from './localStorage'
 
 export const API_URL = import.meta.env.PROD
-  ? 'https://159.223.36.171/api'
+  ? 'https://api.airvnv.info/api'
   : 'http://0.0.0.0:3001/api'
 
 export async function apiGet<D>(resource: string, authToken?: string) {
