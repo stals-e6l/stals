5121996f5b2aa0d14b29ca148b48cca30fcd0c68
Von Divino <93812448+vrfdivino@users.noreply.github.com>
2023-06-01 01:46:57 +0800

    refactor: AccommodationResults

--- frontend/src/modules/accommodation/AccommodationResults.tsx
+++ frontend/src/modules/accommodation/AccommodationResults.tsx
@@ -1,18 +1,39 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, useTheme } from '@mui/material'
 import React from 'react'
-import { retrieveAccommodations } from './AccommodationsProvider'
 import AccommodationCard from './AccommodationCard'
 import { useLocation } from 'react-router-dom'
 import { ROUTES } from '../../app/AppRouter'
+import { apiGet } from '../../services/api'
+import { getToken } from '../../services/localStorage'
+import Title from './TitleComponent'
 
 interface IProps {
   children?: React.ReactNode
   isPublicView?: boolean
+  endpoint?: string
+  title?: string
 }
 
-const AccommodationResults: React.FC<IProps> = () => {
+const AccommodationResults: React.FC<IProps> = ({
+  endpoint = 'public_accommodation?limit=6',
+  title = 'Most viewed',
+}) => {
   const location = useLocation()
-  const accommodations = retrieveAccommodations()
+  const theme = useTheme()
+  const [accommodations, setAccommodations] = React.useState<IAccommodation[]>(
+    []
+  )
+
+  const fetchAccommodations = async () => {
+    const res = await apiGet<IAccommodation[]>(endpoint, getToken() as string)
+    if (res.success && res.data) {
+      setAccommodations(res.data)
+    }
+  }
+
+  React.useEffect(() => {
+    fetchAccommodations()
+  }, [])
 
   if (!accommodations) {
     return (
@@ -28,35 +49,42 @@ const AccommodationResults: React.FC<IProps> = () => {
     <React.Fragment>
       <Grid
         container
-        rowGap={3}
-        columnGap={3}
+        rowGap={2}
         sx={{
-          justifyContent: 'center',
-          transition: '0.3s all',
+          paddingLeft: '10%',
+          paddingRight: '10%',
+          [theme.breakpoints.down('md')]: {
+            padding: '0% 4%',
+          },
+          [theme.breakpoints.down('sm')]: {
+            padding: '0% 2%',
+            paddingTop: '5%',
+          },
         }}
       >
-        {accommodations.map((accommodation, key: number) => (
-          <Grid item key={key} lg={3}>
-            <AccommodationCard
-              accommodation={accommodation}
-              isPublicView={location.pathname === ROUTES.public}
-            />
-          </Grid>
-        ))}
-
-        {/* Fillers */}
-        {}
-        <Grid item lg={3} visibility={'hidden'}>
-          <AccommodationCard
-            accommodation={accommodations[0]}
-            isPublicView={location.pathname === ROUTES.public}
-          />
+        <Grid item>
+          <Title text={title} />
         </Grid>
-        <Grid item lg={3} visibility={'hidden'}>
-          <AccommodationCard
-            accommodation={accommodations[0]}
-            isPublicView={location.pathname === ROUTES.public}
-          />
+
+        <Grid item>
+          <Grid
+            container
+            rowGap={3}
+            columnGap={3}
+            sx={{
+              justifyContent: 'center',
+              transition: '0.3s all',
+            }}
+          >
+            {accommodations.map((accommodation, key: number) => (
+              <Grid item key={key} lg={3}>
+                <AccommodationCard
+                  accommodation={accommodation}
+                  isPublicView={location.pathname === ROUTES.public}
+                />
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
     </React.Fragment>
--- frontend/src/pages/ExplorePage.tsx
+++ frontend/src/pages/ExplorePage.tsx
@@ -1,8 +1,6 @@
-import { Grid, useTheme } from '@mui/material'
 import React from 'react'
 import Banner from '../modules/general/Banner'
 import AccommodationResults from '../modules/accommodation/AccommodationResults'
-import Title from '../modules/accommodation/TitleComponent'
 import AccommodationFormModal from '../modules/accommodation/AccommodationFormModal'
 
 interface IProps {
@@ -10,35 +8,17 @@ interface IProps {
 }
 
 const ExplorePage: React.FC<IProps> = () => {
-  const theme = useTheme()
-
   return (
     <>
       <AccommodationFormModal />
       <Banner />
       <br />
-      <Grid
-        container
-        rowGap={2}
-        sx={{
-          paddingLeft: '10%',
-          paddingRight: '10%',
-          [theme.breakpoints.down('md')]: {
-            padding: '0% 4%',
-          },
-          [theme.breakpoints.down('sm')]: {
-            padding: '0% 2%',
-            paddingTop: '5%',
-          },
-        }}
-      >
-        <Grid item>
-          <Title text="Most Viewed" />
-        </Grid>
-        <Grid item>
-          <AccommodationResults isPublicView={false} />
-        </Grid>
-      </Grid>
+      <AccommodationResults isPublicView={false} />
+      <AccommodationResults
+        isPublicView={false}
+        endpoint="public_accommodation?limit=6"
+        title="Hottest pick"
+      />
     </>
   )
 }
